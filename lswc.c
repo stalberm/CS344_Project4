ca3ff6b1b205135e95bd94fcfd69d147bada36b7
Micah Stalberg <stalberm@oregonstate.edu>
2023-02-01 09:56:03 -0800

    Completes part 1

---
+++ lswc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    char *directory = argv[1];
+    int pfd[2];
+    pipe(pfd);
+    pid_t pid = fork();
+    if (pid == 0) {
+        dup2(pfd[0], 0);
+        close(pfd[1]);
+        execlp("wc", "wc", "-l", NULL);
+    } else {
+        dup2(pfd[1], 1);
+        close(pfd[0]);
+        execlp("ls", "ls", "-1a", directory, NULL);
+    }
+    return 0;
+}
