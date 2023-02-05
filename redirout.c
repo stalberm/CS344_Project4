4db9f8b8687551ee9b9f865d2980197ff09c0595
Micah Stalberg <stalberm@oregonstate.edu>
2023-02-05 14:21:22 -0800

    Extracts functions for redirout and updates line spacing, updates gitignore too

--- redirout.c
+++ redirout.c
@@ -3,10 +3,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+
+void run_child_process(int pfd[2], char *command, char *argv[]);
+void write_to_outfile(int pfd[2], int fd);
+
+
 int main(int argc, char *argv[]) {
 
     if (argc < 3) {
         printf("Too few arguments. Usage: redirout outfilename command [option1] ...\n");
+        exit(1);
     }
 
     char *outfile = argv[1];
@@ -14,31 +20,42 @@ int main(int argc, char *argv[]) {
 
     int fd;
     fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
     int pfd[2];
     pipe(pfd);
+
     pid_t pid = fork();
     if (pid == 0) {
-        dup2(pfd[1], 1);
-        close(pfd[0]);
-        execvp(command, argv+2);
-        perror(command);
-        exit(1);
+        run_child_process(pfd, command, argv);
     } else {
-        close(pfd[1]);
-        char buf[2048];
-        int count;
-        while(1) {
-            count = read(pfd[0], buf, sizeof buf);
-            if (count == 0) {
-                break;
-            } else if (count == -1) {
-                perror("Open");
-                exit(1);
-            }
-            write(fd, buf, count);
-        }
-        close(pfd[0]);
-        close(fd);
+        write_to_outfile(pfd, fd);
     }
     return 0;
+}
+
+
+void write_to_outfile(int pfd[2], int fd) {
+    close(pfd[1]);
+    char buf[2048];
+    int count;
+    while(1) {
+        count = read(pfd[0], buf, sizeof buf);
+        if (count == 0) {
+            break;
+        } else if (count == -1) {
+            perror("Open");
+            exit(1);
+        }
+        write(fd, buf, count);
+    }
+    close(pfd[0]);
+    close(fd);    
+}
+
+void run_child_process(int pfd[2], char *command, char *argv[]) {
+    dup2(pfd[1], 1);
+    close(pfd[0]);
+    execvp(command, argv+2);
+    perror(command);
+    exit(1);
 }
